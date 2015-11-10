741e3e2c9133b4c0b3d6fdfcf9c60ee94fe57434
Keyu Lai <keyu.lai@columbia.edu>
2015-11-10 05:50:17 -0500

    system call for debugging

--- flo-kernel/kernel/sched/core.c
+++ flo-kernel/kernel/sched/core.c
@@ -8355,6 +8355,22 @@ struct cgroup_subsys cpuacct_subsys = {
 
 SYSCALL_DEFINE1(get_wrr_info, struct wrr_info __user *, wrr_info)
 {
+	int cpu_tmp;
+	int cpu_cnt = 0;
+	struct wrr_info buf;
+	struct wrr_rq wrr_rq;
+
+	rcu_read_lock();
+	for_each_possible_cpu(cpu_tmp) {
+		wrr_rq = cpu_rq(cpu_tmp)->wrr;
+		buf.nr_running[cpu_tmp] = wrr_rq.nr_running;
+		buf.total_weight[cpu_tmp] = wrr_rq.total_weight;
+		cpu_cnt++;
+	}
+	rcu_read_unlock();
+	buf.num_cpus = cpu_cnt;
+	if (copy_to_user(wrr_info, &buf, sizeof(struct wrr_info)))
+		return -EINVAL;	
 	return 0;
 }
 
